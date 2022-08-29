b5ae33733cf3bff91784fba11be6ee2fca26ec27
cikava <hembitski@outlook.com>
2022-08-29 21:13:54 +0300

    feat: new features

---
+++ data/difficulties.js
@@ -0,0 +1,24 @@
+const difficulties = [
+    {
+      id: 0,
+      name: 'very-easy'
+    },
+    {
+      id: 1,
+      name: 'easy'
+    },
+    {
+      id: 2,
+      name: 'normal'
+    },
+    {
+      id: 3,
+      name: 'hard'
+    },
+    {
+      id: 4,
+      name: 'very-hard'
+    },
+  ]
+  
+  export default difficulties
---
+++ data/mythicCards/blue/index.js
@@ -0,0 +1,77 @@
+
+const blueCardsData = [
+    {
+      id: 1,
+      cardFace: './assets/MythicCards/blue/blue1.png',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 2,
+      cardFace: './assets/MythicCards/blue/blue2.png',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 3,
+      cardFace: './assets/MythicCards/blue/blue3.png',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 4,
+      cardFace: './assets/MythicCards/blue/blue4.png',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 5,
+      cardFace: './assets/MythicCards/blue/blue5.png',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 6,
+      cardFace: './assets/MythicCards/blue/blue6.png',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 7,
+      cardFace: './assets/MythicCards/blue/blue7.png',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 8,
+      cardFace: './assets/MythicCards/blue/blue8.png',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 9,
+      cardFace: './assets/MythicCards/blue/blue9.png',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 10,
+      cardFace: './assets/MythicCards/blue/blue10.png',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 11,
+      cardFace: './assets/MythicCards/blue/blue11.png',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 12,
+      cardFace: './assets/MythicCards/blue/blue12.png',
+      difficulty: 'normal',
+      color:'blue'
+    },
+  ]
+  
+  export default blueCardsData
