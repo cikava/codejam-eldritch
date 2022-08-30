5b77ba9a951be48a7a5233fd78899bbf9498dc2d
cikava <hembitski@outlook.com>
2022-08-30 16:09:52 +0300

    feat: new features

--- assets/Ancients/index.js
+++
@@ -1,8 +0,0 @@
-const ancients = {
-    azathoth: './assets/Ancients/Azathoth.png',
-    cthulhu: './assets/Ancients/Cthulthu.png',
-    iogSothoth: './assets/Ancients/IogSothoth.png',
-    shubNiggurath: './assets/Ancients/ShubNiggurath.png',
-  }
-  
-  export default ancients
